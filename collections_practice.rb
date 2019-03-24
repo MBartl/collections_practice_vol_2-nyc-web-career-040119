b98ea2934206917d43ada2cba2cd682afcc7aa86
Matthew Bartl <mattbartl@gmail.com>
2019-03-24 04:00:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -64,6 +64,7 @@ def merge_data(keys, data)
   data.each do |both|
     both.each do |name, stats|
       i = 0
+      while i < keys.length
       if keys[i][:first_name] == name
         stats[:first_name] = keys[i][:first_name]
         stats[:motto] = keys[i][:motto]
