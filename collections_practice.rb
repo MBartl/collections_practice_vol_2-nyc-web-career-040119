b5e70ae4b768908feab89b36eaf64c422c004849
Matthew Bartl <mattbartl@gmail.com>
2019-03-24 03:32:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -63,8 +63,7 @@ def merge_data(keys, data)
   output = []
   data.each do |both|
     both.each do |name, stats|
-      keys do |info|
-        output.push(info, stats)
+        output.push(stats)
       end
     end
   end
