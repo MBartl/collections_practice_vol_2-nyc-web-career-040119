aadd88d442b256d52b7efe7e0a4cc915cb24362a
Matthew Bartl <mattbartl@gmail.com>
2019-03-24 04:09:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -82,6 +82,7 @@ end
 def find_cool(array)
   array.each do |data|
     data.each do |key, value|
+      return value[:temperature]
       if value[:temperature] == "cool"
         return value
       end
